f68ab67a04a2fcf22d48e21b04dec2060ab463d2
DakshAariwalaa <136902138+DakshAariwalaa@users.noreply.github.com>
2023-11-02 17:47:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('save.png');
+}
